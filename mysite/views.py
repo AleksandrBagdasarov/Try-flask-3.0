36a284002d01b5135c5dbaab303453b3ffacff7f
Aleksander <sashabagdasarov@gmail.com>
2020-06-23 22:17:30 +0300

    comiit

--- mysite/views.py
+++ mysite/views.py
@@ -2,14 +2,38 @@ from flask import redirect, render_template, request, url_for
 from mysite.models import Product, Tag
 
 
-def home():
-    q = request.args.get('q')
-    if q:
-        products = Product.query.filter(Product.title.contains(q)).all()
-    else:
-        products = Product.query.all()
+def home(previous_sort=None, sorted_by=None):
     tags = Tag.query.all()
-    return render_template('home.html', title='Home', products=products, tags=tags)
+    q = request.args.get('q')
+    if q or sorted_by:
+        if q:
+            products = Product.query.filter(Product.title.contains(q))
+            previous_sort = products
+        if sorted_by and previous_sort:
+            if sorted_by == 'top':
+                pass
+            elif sorted_by == 'featured':
+                products = previous_sort.order_by(Product.id)
+
+            elif sorted_by == 'low':
+                pass
+            elif sorted_by == 'high':
+                pass
+        elif sorted_by:
+            if sorted_by == 'top':
+                pass
+            elif sorted_by == 'featured':
+                products = Product.query.order_by(Product.id)
+
+            elif sorted_by == 'low':
+                pass
+            elif sorted_by == 'high':
+                pass      
+    else:
+        products = Product.query
+
+    products = products.all()    
+    return render_template('home.html', title='Home', products=products, tags=tags, previous_sort=previous_sort)
 
 
 def product_details(slug):
